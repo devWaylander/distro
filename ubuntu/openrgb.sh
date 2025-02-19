b03a7a654f3439d107cb8a39cfc7000e18306fef
Pavel Chubakov <dev.waylander@gmail.com>
2025-02-19 14:58:40 +0700

    feat: openrgb.sh && update.sh

--- ubuntu/openrgb.sh
+++ ubuntu/openrgb.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+set -e  # Прерываем выполнение при ошибке
 
-# Скачиваем .deb пакет
-wget https://openrgb.org/releases/release_0.9/openrgb_0.9_amd64_bullseye_b5f46e3.deb
+sudo apt update && sudo apt upgrade
 
-# Устанавливаем .deb пакет
-sudo dpkg -i openrgb_0.9_amd64_bullseye_b5f46e3.deb
+# Хардкод последней версии OpenRGB для Bullseye
+LATEST_URL="https://openrgb.org/releases/release_0.9/openrgb_0.9_amd64_bookworm_b5f46e3.deb"
+DEB_PACKAGE="openrgb_0.9_amd64_bookworm_b5f46e3.deb"
 
-# Если возникли проблемы с зависимостями, исправляем их
-sudo apt --fix-broken install -y
+# Скачиваем deb-файл
+wget "$LATEST_URL"
 
-# Удаляем скачанный .deb пакет после установки
-rm -f openrgb_0.9_amd64_bullseye_b5f46e3.deb
+# Устанавливаем OpenRGB
+sudo apt --fix-broken install "./$DEB_PACKAGE" 
+
+# Проверяем успешную установку
+if dpkg -s openrgb >/dev/null 2>&1; then
+    echo "Установка OpenRGB завершена! Запустить можно командой: openrgb"
+else
+    echo "Ошибка: OpenRGB не был установлен. Проверьте зависимости."
+fi
+
+# Очистка
+rm -f "./$DEB_PACKAGE"
